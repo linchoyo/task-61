409484e74c1863ed22b6c1364c761b03d2651e2f
linchoyo <91781279+linchoyo@users.noreply.github.com>
2022-12-20 11:46:35 +0200

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,13 @@
 import "../scss/app.scss";
 
+const collection = [...document.getElementsByClassName("hot")];
+
+for (let i = 0; i <= collection.length; i++) {
+  if(collection[i].classList.contains("hot")) {
+    collection[i].innerHTML += '🔥';
+  }
+}
+
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
 });
